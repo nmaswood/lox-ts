550604b3e11fab9d696d76b39e92063f76b94884
Nasr.Maswood <nasrmaswood@gmail.com>
2020-08-02 19:59:46 -0400

    start fixing unit tests;

--- __tests__/scan.test.ts
+++ __tests__/scan.test.ts
@@ -3,14 +3,17 @@ import * as E from "fp-ts/lib/Either";
 import { scan } from "../src/scanner/scan";
 
 const CASES: {
+  description: string;
   input: string;
   expected: ReturnType<typeof scan>;
 }[] = [
   {
+    description: "empty input",
     input: "",
     expected: E.right([]),
   },
   {
+    description: "variable assigned to number",
     input: "var x = 10",
     expected: E.right([
       {
@@ -50,21 +53,216 @@ const CASES: {
     ]),
   },
   {
-    input: "",
-    expected: E.right([]),
+    description: "varible assigned to string",
+    input: 'var x = "hello world"',
+    expected: E.right([
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "VAR",
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "literal",
+          value: {
+            kind: "IDENTIFIER",
+            value: "x",
+          },
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "EQUAL",
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "literal",
+          value: {
+            kind: "STRING",
+            value: "hello world",
+          },
+        },
+      },
+    ]),
   },
   {
-    input: "",
-    expected: E.right([]),
+    description: "simple variable expression",
+    input: "x = x - 1",
+    expected: E.right([
+      {
+        line: 0,
+        token: {
+          type: "literal",
+          value: {
+            kind: "IDENTIFIER",
+            value: "x",
+          },
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "EQUAL",
+        },
+      },
+
+      {
+        line: 0,
+        token: {
+          type: "literal",
+          value: {
+            kind: "IDENTIFIER",
+            value: "x",
+          },
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "MINUS",
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "literal",
+          value: {
+            kind: "NUMBER",
+            value: 1,
+          },
+        },
+      },
+    ]),
   },
   {
-    input: "",
-    expected: E.right([]),
+    description: "empty function",
+    input: "func identifier(){}",
+    expected: E.right([
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "FUN",
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "literal",
+          value: {
+            kind: "IDENTIFIER",
+            value: "identifier",
+          },
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "LEFT_PAREN",
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "RIGHT_PAREN",
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "LEFT_BRACE",
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "RIGHT_BRACE",
+        },
+      },
+    ]),
+  },
+  {
+    description: "empty function across new lines",
+    input: ` function identifier()
+        {
+          }`,
+    expected: E.right([
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "FUN",
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "literal",
+          value: {
+            kind: "IDENTIFIER",
+            value: "identifier",
+          },
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "LEFT_PAREN",
+        },
+      },
+      {
+        line: 0,
+        token: {
+          type: "non_literal",
+          kind: "RIGHT_PAREN",
+        },
+      },
+      {
+        line: 1,
+        token: {
+          type: "non_literal",
+          kind: "LEFT_BRACE",
+        },
+      },
+      {
+        line: 2,
+        token: {
+          type: "non_literal",
+          kind: "RIGHT_BRACE",
+        },
+      },
+    ]),
+  },
+  {
+    description: "displays a number error",
+    input: ` var x = 10
+    3.1a.  `,
+    expected: E.left([
+      {
+        line: 1,
+        message: "TO DO",
+      },
+    ]),
   },
 ];
 
 describe("scan", () => {
-  CASES.forEach(({ input, expected }) => {
-    expect(scan(input)).toEqual(expected);
+  CASES.forEach(({ input, expected, description }) => {
+    it(`correctly handles ${description}`, () =>
+      expect(scan(input)).toEqual(expected));
   });
 });
